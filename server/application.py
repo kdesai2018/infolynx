02919992bb3dedf0683f66feba1cedb34c903bae
Didi Zhou <didizhou@Didis-MacBook-Pro.local>
2020-09-19 14:49:17 -0500

    pulled get keywords into new function

--- server/application.py
+++ server/application.py
@@ -43,21 +43,22 @@ def get_video_info():
 
     print(timed_transcript)
 
+    return getKeywords(transcript_url)
+
+def getKeywords(transcript_url):
     #IBM Watson NLU
-    # authenticator = IAMAuthenticator('{apikey}')
-    # natural_language_understanding = NaturalLanguageUnderstandingV1(
-    #     version='2020-08-01',
-    #     authenticator=authenticator
-    # )
+    authenticator = IAMAuthenticator('TWS446L2CH4Zxnrh-nwh3T2g8stRlB08e4iyjAKyBHg0')
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2020-08-01',
+        authenticator=authenticator
+    )
 
-    # natural_language_understanding.set_service_url('{url}')
+    natural_language_understanding.set_service_url('https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/816f28bc-9729-48ca-b11a-c736524e6ad6')
 
-    # response = natural_language_understanding.analyze(
-    #     url=transcript_url,
-    #     features=Features(keywords=KeywordsOptions(sentiment=False,emotion=False,limit=5))).get_result()
+    response = natural_language_understanding.analyze(
+        url=transcript_url,
+        features=Features(keywords=KeywordsOptions(sentiment=False,emotion=False,limit=5))).get_result()
 
-    # print(json.dumps(response, indent=2))
+    return response
 
-    return url
-
-get_video_info()
+getKeywords("http://video.google.com/timedtext?lang=en&v=aircAruvnKk")
